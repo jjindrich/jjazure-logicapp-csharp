a505935f71d63724d1653705a8e4742ec042054e
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2021-05-04 12:21:59 +0000

    save connection string

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -21,13 +21,15 @@ namespace JJ.Function
 
             string name = req.Query["name"];
 
+            string connstr = "Server=tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID=mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;";
+
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello JJ, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
